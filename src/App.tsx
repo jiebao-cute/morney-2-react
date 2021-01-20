44efd009b8178880bdb66ff7419537c3d7a82896
jiebao <908822571@qq.com>
2021-01-20 23:18:31 +0800

    React Router CRM  和flex布局来定位导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,88 @@
 import React from 'react';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div>
+const Wrapper = styled.div`
+height: 100vh;
+display: flex;
+flex-direction: column;
+`
+const Main = styled.div`
+flex-grow: 1;
+overflow: auto;
+`
+const Nav = styled.div`
+border: 1px solid rebeccapurple;
+>ul{
+display: flex;
+  >li{
+  width: 33.3333%;
+  text-align: center;
+  padding: 16px;
+  }
+}
 
-    </div>
-  );
+`
+ function App() {
+   return (
+     <Router>
+       <Wrapper>
+         <Main>
+           <Switch>
+             <Route path="/tags">
+               <Tags/>
+             </Route>
+             <Route path="/money">
+               <Money/>
+             </Route>
+             <Route path="/statistics">
+               <Statistics/>
+             </Route>
+             <Redirect exact from="/" to="/money"/>
+             <Route path="*">
+               <NoMatch />
+             </Route>
+           </Switch>
+         </Main>
+         <Nav>
+           <ul>
+             <li>
+               <Link to="/tags">标签</Link>
+             </li>
+             <li>
+               <Link to="/money">记账</Link>
+             </li>
+             <li>
+               <Link to="/statistics">统计</Link>
+             </li>
+           </ul>
+         </Nav>
+       </Wrapper>
+     </Router>
+   );
+}
+function Statistics() {
+  return <h2>统计</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch(){
+  return(
+    <div>页面不存在，地址错啦</div>
+  )
 }
 
 export default App;
