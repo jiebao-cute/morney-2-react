b7798227bfe3a45f8dcc6686c753f1486d39316d
jiebao <908822571@qq.com>
2021-01-21 17:06:01 +0800

    创建Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,11 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-height: 100vh;
-display: flex;
-flex-direction: column;
-`
-const Main = styled.div`
-flex-grow: 1;
-overflow: auto;
-`
+import Layout from './components/Layout';
 
  function App() {
    return (
      <Router>
-       <Wrapper>
-         <Main>
            <Switch>
              <Route path="/tags">
                <Tags/>
@@ -38,22 +25,31 @@ overflow: auto;
                <NoMatch />
              </Route>
            </Switch>
-         </Main>
-         <Nav/>
-       </Wrapper>
      </Router>
    );
 }
 function Statistics() {
-  return <h2>统计</h2>;
+  return(
+    <Layout>
+      <h2>统计</h2>
+    </Layout>
+    )
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return(
+     <Layout>
+        <h2>标签</h2>
+     </Layout>
+)
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return(
+    <Layout>
+       <h2>记账</h2>
+    </Layout>
+  )
 }
 
 function NoMatch(){
