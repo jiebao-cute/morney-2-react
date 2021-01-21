ef47884ecdd5783a46f0db44210fdb04472257d5
jiebao <908822571@qq.com>
2021-01-21 16:07:12 +0800

    创建ICon组件

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-require('icons/money.svg');
-require('icons/chart.svg');
-require('icons/tag.svg');
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try{
+  importAll(require.context('icons',true, /\.svg$/));}catch(error) {console.log(error);}
 
 type Props = {
   name: string
