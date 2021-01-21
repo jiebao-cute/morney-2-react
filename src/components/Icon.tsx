80f707f05f03fe032249a34489414033623569f3
jiebao <908822571@qq.com>
2021-01-21 15:45:29 +0800

    Icon重复代码模块化

---
+++ src/components/Icon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+require('icons/money.svg');
+require('icons/chart.svg');
+require('icons/tag.svg');
+
+type Props = {
+  name: string
+}
+
+const Icon =(props:Props)=> {
+  return (
+    <svg className="icon">
+      <use xlinkHref={'#'+ props.name} />
+    </svg>
+  );
+};
+
+export default Icon;
