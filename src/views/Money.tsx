31e51983ffbfbb6dc3cbf124a9dae20e64729bb9
jiebao <908822571@qq.com>
2021-01-27 16:39:24 +0800

    money之TagsSection UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,7 +2,25 @@ import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
 const TagsSection = styled.section`
-
+background: #FFFFFF;
+padding: 12px 16px;
+>ol{margin: 0 -12px;
+  >li{
+    background: #D9D9D9;
+    border-radius: 18px;
+    display: inline-block;
+    padding: 3px 18px;
+    font-size: 14px;
+    margin: 8px 12px;
+    }
+  }
+  >button{
+    background: none;
+    border: none;
+    border-bottom:1px solid;
+    color: #666;
+    padding: 2px 4px;
+    margin-top: 8px;
 `
 const NotesSection = styled.section`
 
