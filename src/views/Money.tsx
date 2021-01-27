b0323552cfd0e7194ddffcea72a2d1061a7ab5e5
jiebao <908822571@qq.com>
2021-01-28 00:24:20 +0800

    Money 整个UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import styled from 'styled-components';
 const TagsSection = styled.section`
 background: #fff8dd;
+flex-grow: 1;
+display: flex;
+flex-direction: column;
+justify-content: flex-end;
+align-items: flex-start;
 padding: 12px 16px;
 >ol{margin: 0 -12px;
   >li{
@@ -113,9 +118,14 @@ const NumberPadSection = styled.section`
     }
     }
 `
+const MyLayout = styled(Layout)`
+display: flex;
+flex-direction: column;
+`
+
 function Money() {
   return(
-    <Layout>
+    <MyLayout>
        <TagsSection>
          <ol>
            <li>衣</li>
@@ -158,7 +168,7 @@ function Money() {
              <button>.</button>
            </div>
          </NumberPadSection>
-    </Layout>
+    </MyLayout>
   )
 }
 export default Money;
