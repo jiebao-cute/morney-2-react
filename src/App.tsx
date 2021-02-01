38c890332b4413573a5663d31240ce5003140509
jiebao <908822571@qq.com>
2021-02-01 19:30:56 +0800

    创建 /tags/:tag 路由，学会 exact 的用法

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
 import styled from 'styled-components';
+import {Tag} from './views/Tag';
 
 const AppWrapper = styled.div`
 color: #333;
@@ -21,13 +22,16 @@ color: #333;
      <AppWrapper>
      <Router>
            <Switch>
-             <Route path="/tags">
+             <Route exact path="/tags/:tag">
+               <Tag/>
+             </Route>
+             <Route exact path="/tags">
                <Tags/>
              </Route>
-             <Route path="/money">
+             <Route exact path="/money">
                <Money/>
              </Route>
-             <Route path="/statistics">
+             <Route exact path="/statistics">
                <Statistics/>
              </Route>
              <Redirect exact from="/" to="/money"/>
---
+++ src/views/Tag.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Tag:React.FC = ()=>{
+  return (
+    <Layout>
+    <div>
+      你好
+    </div>
+    </Layout>
+  )
+}
+export {Tag}
