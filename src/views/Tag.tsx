5f5488d8ab140f808c8ffff2529217010f5883a0
jiebao <908822571@qq.com>
2021-02-01 22:22:12 +0800

    封装 findTag 到 useTags 里

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Layout from 'components/Layout';
-
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
+type Params ={
+  id:string
+}
 const Tag:React.FC = ()=>{
+  const {findTag} = useTags();
+  let{id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
   return (
     <Layout>
     <div>
-      你好
+      {tag.name}
     </div>
     </Layout>
   )
