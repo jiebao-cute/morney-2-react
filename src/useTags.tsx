3315eea52283a5c94b4af942aa74b7a78edabdf8
jiebao <908822571@qq.com>
2021-02-02 20:37:39 +0800

    重构：tag页面的删除标签实现代码更简洁

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -19,23 +19,11 @@ const useTags =() =>{
      }
     return result;
   }
-   const updateTag = (id:number ,obj:{name:string}) => {
-    //获取要改的id的下标
-    const index = findTagIndex(id);
-    //深拷贝tags到tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //把tagsClone的第index删掉，换成{id:id,name:obj.name}
-    tagsClone.splice(index,1,{id:id , name: obj.name});
-    setTags(tagsClone);
+   const updateTag = (id:number ,{name}:{name:string}) => {
+    setTags(tags.map(tag => tag.id === id ? {id , name:name} : tag));
    }
    const deleteTag = (id:number)=>{
-     //获取要改的id的下标
-     const index = findTagIndex(id);
-     //深拷贝tags到tagsClone
-     const tagsClone = JSON.parse(JSON.stringify(tags));
-     //把tagsClone的第index删掉
-     tagsClone.splice(index,1);
-     setTags(tagsClone);
+     setTags(tags.filter(tag=>tag.id !== id));
    }
 
   return {tags,setTags,findTag,findTagIndex,updateTag,deleteTag}
