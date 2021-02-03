c317e09aa49074a6d98dad22e97101b8883e9d3f
jiebao <908822571@qq.com>
2021-02-03 18:20:07 +0800

    数据传入localStorage里

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,13 +1,24 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {createId} from 'lib/createId';
-const defaultTags=[
-  {id:createId(),name:'衣服'},
-  {id:createId(),name:'吃饭饭'},
-  {id:createId(),name:'家庭消费'},
-  {id:createId(),name:'粗去玩~'},
-]
-const useTags =() =>{
-  const [tags,setTags] = useState<{id:number ; name:string}[]>(defaultTags);
+import {useUpdate} from 'components/hooks/useUpdate';
+
+const useTags =() =>{//封装一个自定义hook
+  const [tags,setTags] = useState<{id:number ; name:string}[]>([]);
+  useEffect(()=>{
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]')
+    if (localTags.length ===0){
+      localTags = [
+        {id:createId(),name:'衣服'},
+        {id:createId(),name:'吃饭饭'},
+        {id:createId(),name:'家庭消费'},
+        {id:createId(),name:'粗去玩~'},
+      ]
+    }
+    setTags(localTags);
+  },[]);//deps是空数组代表第一次进来就执行
+  useUpdate(()=>{
+    window.localStorage.setItem('tags', JSON.stringify(tags))
+  },[tags]);
   const findTag = (id:number)=>tags.filter(tag => tag.id === id)[0];
   const findTagIndex =(id:number)=>{
      let result = -1;
@@ -28,7 +39,11 @@ const useTags =() =>{
   const addTag = ()=>{
     const tagName = window.prompt('新标签的名字为');
     if(tagName !== null){
-      setTags([...tags,{id:createId(),name:tagName}])
+      if (tagName === '') {
+        window.prompt('新标签的名字不可以是空的哦~');
+      } else {
+        setTags([...tags, {id: createId(), name: tagName}]);
+      }
     }
   };
   return {tags,setTags,findTag,findTagIndex,updateTag,deleteTag,addTag}
